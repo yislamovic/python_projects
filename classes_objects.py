8d0feaca41a5447c645cfa2bce9ac8d324bc03f1
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-10-25 18:43:06 -0400

    added extra feature to mentor object

--- classes_objects.py
+++ classes_objects.py
@@ -1,18 +1,17 @@
 class Student():
-  def __init__(self, name, major, gpa):
-    self.name = name
-    self.major = major
-    self.gpa = gpa
-  
-  def honor_roll(self):
-    if self.gpa >= 3.5:
-      return True
-    else:
-      return False
+    def __init__(self, name, major, gpa):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
 
-  def _probation(self):
-    if self.gpa >= 2.0:
-      return False
-    else:
-      return True
+    def honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
 
+    def _probation(self):
+        if self.gpa >= 2.0:
+            return False
+        else:
+            return True
--- inheritance.py
+++ inheritance.py
@@ -1,20 +1,28 @@
 from classes_objects import Student
+
+
 class Mentor(Student):
-  def __init__(self, name, major, gpa):
-      super().__init__(name, major, gpa)
+    def __init__(self, name, major, gpa, subject):
+        super().__init__(name, major, gpa)
+        self.subject = subject
 
-  def is_elligable_mentor(self):
-    if self._probation:
-      return False
-    else: 
-      return True
-
+    def is_elligable_mentor(self):
+        if self._probation():
+            return False
+        else:
+            return True
 
 
 student1 = Student('Yahya', 'CompSci', 3.5)
 student2 = Student('Job', 'Marketing', 1.6)
-mentor1 = Mentor('Yahya', 'CompSci', 3.5)
+
+mentor1 = Mentor('Yahya', 'CompSci', 3.5, 'math')
+mentor2 = Mentor('Job', 'Marketing', 1.6, 'politics')
 
 print(student1.honor_roll())
 print(student2.honor_roll())
+
 print(mentor1.is_elligable_mentor())
+print(mentor2.is_elligable_mentor())
+
+print(mentor2.subject)
